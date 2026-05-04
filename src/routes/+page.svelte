596a20a685227da3d8bc7c940d1d7f924a24fb20
mellunar <melfer100@gmail.com>
2026-05-04 10:18:18 -0300

    feat: homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="e-modal e-modal--alert">
+	<div class="e-modal__icon">
+		<span style="width: 32px;">
+			<img alt="" draggable="false" src="/mac/apple-black.svg" />
+		</span>
+	</div>
+
+	<h4 class="e-modal__title">Mac OS Window Generator</h4>
+
+	<p>
+		This project was created to generate Mac windows as imagens to be used in a video of mine where I present
+		images as if they were screenshoted from a Mac OS screen.
+	</p>
+
+	<p>
+		I was always passionate about Mac OS system design, although I only had a Snow Leopard running on a old laptop.
+		Anyway, the project is open and has no envolvement from Apple.
+	</p>
+
+	<p>The current version is a mix of Ventura and Sonoma.</p>
+
+	<div class="e-modal__footer e-modal__footer-end">
+		<button class="button button-primary" disabled={true}>GitHub</button>
+	</div>
+</div>
